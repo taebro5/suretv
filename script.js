998d467a6d1c5060175b0c89899e017fe5d16f60
taebro5 <79618842+taebro5@users.noreply.github.com>
2023-07-06 12:16:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+ <script src="http://mattstow.com/experiment/responsive-image-maps/jquery.rwdImageMaps.min.js"></script>
+ <script>
+$(function(){
+	$('img[usemap]').rwdImageMaps();
+	$("#img").width("100%");
+});
+</script>
